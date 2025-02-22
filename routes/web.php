dc12e08c3fa48065ef2cd7a04111a4cf68c5055c
daprett <athallahdaffa936@gmail.com>
2025-02-22 10:48:35 +0700

    PWL Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +25,59 @@ Route::get('/', function () { // Menampilkan halaman 'welcome' saat pengguna men
 });
 
 Route::resource('items', ItemController::class);// Menyediakan rute restful untuk resource 'items, dengan itemcontroller
+
+// Route::get('/hello', function(){
+//     return 'Hello world';
+// });
+
+// Route::get('/world', function(){
+//     return 'World';
+// });
+
+// Route::get('/welcome', function(){
+//     return 'Selamat Datang';
+// });
+
+// Route::get('/about', function(){
+//     return 'Nama : Daffa NIM : 2341760079';
+// });
+
+// Route::get('/user/{name}', function($name){
+//     return 'Nama Saya '.$name;
+// });
+
+// Route::get('/post/{post}/comments/{comment}', function($postId,$commentId){
+//     return 'post ke-'.$postId.' Komentar ke-'.$commentId; 
+// });
+
+// Route::get('articles/{id}', function($id){
+//     return 'Halaman Artikel dengan ID '.$id;
+// });
+
+// route::get('/user/{name?}', function($name=null){
+//     return 'Nama saya '.$name;
+// });
+
+// route::get('/user/{name?}', function($name='jonh'){
+//     return 'Nama saya '. $name;
+// });
+
+// route::get('/hello', [WelcomeController::class,'hello']);
+
+// route::get('/',[PageController::class,'index']);
+// route::get('/about',[PageController::class,'about']);
+// route::get('/articles/{id}',[PageController::class,'articles']);
+
+route::get('/',HomeController::class);
+route::get('/about',AboutController::class);
+route::get('/articles/{id}',ArticleController::class);
+
+route::resource('photos',PhotoController::class);
+
+route::resource('photos',PhotoController::class)->only([
+    'index','show'
+]);
+
+route::resource('photos',PhotoController::class)->Exception([
+    'create','store','update','destroy'
+]);
